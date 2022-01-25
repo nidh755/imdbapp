6bd80cd3c0f9d0550ae49affa05f2f8171621fae
nidh755 <nidhikhire86@gmail.com>
2022-01-25 16:29:28 +0530

    commit on 01/22

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,2 @@
+export const APIKey = "6b303d87";
+//generated api through the site
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+     <div>Imdb Movie App</div>
+           <div>©2022, TECHDOME IMDB Inc.</div>
+    </div>
+    //company authority
+  );
+};
+
+export default Footer;
